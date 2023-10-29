4958be1add1699e1be95b2a4afb62d1b12a65bc2
Carrasco Candela E R <candenwnh@gmail.com>
2023-10-29 20:59:08 -0300

    Se colocan las animaciones del Jugador para sus distintos movimientos

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -8,6 +8,9 @@ public class PlayerMove : MonoBehaviour
     public float jumpSpeed=3;
     Rigidbody2D rb2D;
 
+    public SpriteRenderer spriteRenderer;
+    public Animator animator;
+
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
@@ -19,19 +22,34 @@ public class PlayerMove : MonoBehaviour
         if(Input.GetKey("d") || Input.GetKey("right"))
         {
             rb2D.velocity = new Vector2(runSpeed, rb2D.velocity.y);
+            spriteRenderer.flipX = true;
+            animator.SetBool("walk", true);
         }
         
         else if(Input.GetKey("a") || Input.GetKey("left"))
         {
             rb2D.velocity = new Vector2(-runSpeed, rb2D.velocity.y);
+            spriteRenderer.flipX = false;
+            animator.SetBool("walk", true);
         }
         else
         {
             rb2D.velocity = new Vector2(0, rb2D.velocity.y);
+            animator.SetBool("walk", false);
         }
         if(Input.GetKey("space") && checkGround.isGrounded)
         {
             rb2D.velocity = new Vector2(rb2D.velocity.x, jumpSpeed);
+        }
+
+        if(checkGround.isGrounded==false)
+        {   
+            animator.SetBool("jump", true);
+            animator.SetBool("walk", false);
+        }
+        if(checkGround.isGrounded==true)
+        {   
+            animator.SetBool("jump", false);
         }
     }
 }
